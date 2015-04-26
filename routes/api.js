eda9b0e847bdc67c23b5f8d16323da0e8090ff25
user808d <qlcfz5@mst.edu>
2015-04-26 16:06:50 -0500

    users, articles, and edits api

--- routes/api.js
+++ routes/api.js
@@ -10,7 +10,7 @@ api.get('/users/', function(req, res, next) {
             res.send({result: 'error', err: err.code});
         }
         else{
-            var q_string = 'SELECT * FROM Users AS U, Fields AS F WHERE F.fieldID = U.fieldID;';
+            var q_string = 'SELECT * FROM Users U, Fields F WHERE F.fieldID = U.fieldID;';
             
             connection.query(q_string, function(err, rows, fields){
                 if(err){
@@ -23,8 +23,7 @@ api.get('/users/', function(req, res, next) {
                 }
                 else{
                     res.send({
-                        rows: rows,
-                        fields: fields
+                        users: JSON.stringify(rows)
                     });
                 }
             });
@@ -37,19 +36,68 @@ api.get('/users/', function(req, res, next) {
 api.post('/users/', function(req, res, next) {
 });
 
+/* PUT updated user info */
+api.put('/users/:id', function(req, res, next){
+});
+
 /* GET user info by email */
 api.get('/users/:email', function(req, res, next) {
+    db.getConnection(function(err, connection){
+        if(err){
+            console.error('Connection error: ', err);
+            res.send({result: 'error', err: err.code});
+        }
+        else{
+            var q_string = 'SELECT * FROM Users U, Fields F WHERE F.fieldID = U.fieldID AND U.email = ?';
+            var email = req.params.email;
+            
+            connection.query(q_string, [email], function(err, rows, fields){
+                if(err){
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err: err.code
+                    });
+                }
+                else{
+                    res.send({
+                        users: JSON.stringify(rows)                        
+                    });
+                }
+            });
+        }
+        connection.release();
+    });
 });
 
 /* GET articles */
 api.get('/articles/', function(req, res, next) {
     db.getConnection(function(err, connection){
-        var q_string = 'SELECT * FROM Articles A, Abstracts Ab, Users U WHERE A.authorEmail == U.email AND A.abstractID == Ab.abstractID';
-
-        connection.query(q_string, function(err, rows){
-                res.send(rows);
-                connection.release();
-        });
+        if(err){
+            console.error('Connection error: ', err);
+            res.send({result: 'error', err: err.code});
+        }
+        else{
+            var q_string = 'SELECT * FROM Articles A, Abstracts Ab WHERE A.articleID = Ab.articleID';
+            
+            connection.query(q_string, function(err, rows, fields){
+                if(err){
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err: err.code
+                    });
+                }
+                else{
+                    res.send({
+                        articles: JSON.stringify(rows)                        
+                    });
+                }
+            });
+        }
+        connection.release();
     });
 });
 
@@ -59,6 +107,33 @@ api.post('/articles/', function(req, res, next) {
 
 /* GET article by id */
 api.get('/articles/:id', function(req, res, next) {
+    db.getConnection(function(err, connection){
+        if(err){
+            console.error('Connection error: ', err);
+            res.send({result: 'error', err: err.code});
+        }
+        else{
+            var q_string = 'SELECT * FROM Articles A, Abstracts Ab WHERE A.articleID = Ab.articleID AND A.articleID = ?';
+            var id = req.params.id;
+            
+            connection.query(q_string, [id], function(err, rows, fields){
+                if(err){
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err: err.code
+                    });
+                }
+                else{
+                    res.send({
+                        articles: JSON.stringify(rows)                        
+                    });
+                }
+            });
+        }
+        connection.release();
+    });
 });
 
 /* PUT updated article by id */
@@ -67,11 +142,63 @@ api.put('/articles/:id', function(req, res, next) {
 
 /* GET articles by author (email must be encoded) */
 api.get('/articles/:email', function(req, res, next) {
+    db.getConnection(function(err, connection){
+        if(err){
+            console.error('Connection error: ', err);
+            res.send({result: 'error', err: err.code});
+        }
+        else{
+            var q_string = 'SELECT * FROM Articles A, Abstracts Ab WHERE A.articleID = Ab.articleID AND A.authorEmail = ?';
+            var email = req.params.email;
+            
+            connection.query(q_string, [email], function(err, rows, fields){
+                if(err){
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err: err.code
+                    });
+                }
+                else{
+                    res.send({
+                        articles: JSON.stringify(rows)                        
+                    });
+                }
+            });
+        }
+        connection.release();
+    });
 });
 
 /* GET edits */
 api.get('/edits/', function(req, res, next) {
-    res.send('edits page');
+    db.getConnection(function(err, connection){
+        if(err){
+            console.error('Connection error: ', err);
+            res.send({result: 'error', err: err.code});
+        }
+        else{
+            var q_string = 'SELECT * FROM Edits';
+            
+            connection.query(q_string, function(err, rows, fields){
+                if(err){
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err: err.code
+                    });
+                }
+                else{
+                    res.send({
+                        edits: JSON.stringify(rows)                        
+                    });
+                }
+            });
+        }
+        connection.release();
+    });
 });
 
 /* POST user edit to an article */
@@ -80,15 +207,95 @@ api.post('/edits/', function(req, res, next) {
 
 /* GET edits by author */
 api.get('/edits/:email', function(req, res, next) {
+    db.getConnection(function(err, connection){
+        if(err){
+            console.error('Connection error: ', err);
+            res.send({result: 'error', err: err.code});
+        }
+        else{
+            var q_string = 'SELECT * FROM Edits WHERE authorEmail = ?';
+            var email = req.params.email;
+            
+            connection.query(q_string, [email], function(err, rows, fields){
+                if(err){
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err: err.code
+                    });
+                }
+                else{
+                    res.send({
+                        edits: JSON.stringify(rows)                        
+                    });
+                }
+            });
+        }
+        connection.release();
+    });
 });
 
 /* GET edits by date */
 api.get('/edits/:date', function(req, res, next) {
+    db.getConnection(function(err, connection){
+        if(err){
+            console.error('Connection error: ', err);
+            res.send({result: 'error', err: err.code});
+        }
+        else{
+            var q_string = 'SELECT * FROM Edits WHERE date = ?';
+            var date = req.params.date;
+            
+            connection.query(q_string, [date], function(err, rows, fields){
+                if(err){
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err: err.code
+                    });
+                }
+                else{
+                    res.send({
+                        edits: JSON.stringify(rows)                        
+                    });
+                }
+            });
+        }
+        connection.release();
+    });
 });
 
 /* GET edits by article id */
 api.get('/edits/:article_id', function(req, res, next) {
+    db.getConnection(function(err, connection){
+        if(err){
+            console.error('Connection error: ', err);
+            res.send({result: 'error', err: err.code});
+        }
+        else{
+            var q_string = 'SELECT * FROM Edits WHERE date = ?';
+            var article_id = req.params.article_id;
+            
+            connection.query(q_string, [article_id], function(err, rows, fields){
+                if(err){
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err: err.code
+                    });
+                }
+                else{
+                    res.send({
+                        edits: JSON.stringify(rows)                        
+                    });
+                }
+            });
+        }
+        connection.release();
+    });
 });
-
 
 module.exports = api;
