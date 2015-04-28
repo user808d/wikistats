29edfe064b29b0a0e5358d7c6739984ced06dd31
user808d <qlcfz5@mst.edu>
2015-04-27 20:40:52 -0500

    moved select and insert functionality to db module

--- database.js
+++ database.js
@@ -7,4 +7,56 @@ var pool = mysql.createPool({
     database: 'wikistats_db'
 });
 
+pool.select = function(res, queryString, params){
+    pool.getConnection(function(err, connection){
+        if(err){
+            console.error('Connection error: ', err);
+            res.send({result: 'error', err: err.code});
+        }
+        else{            
+            connection.query(queryString, params, function(err, rows, fields){
+                connection.release();
+                if(err){
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err: err.code
+                    });
+                }
+                else{
+                    res.send({
+                        rows: rows
+                    });
+                }
+            });
+        }
+    });
+};
+
+pool.insert = function(res, queryString, values){
+    pool.getConnection(function(err, connection){
+        if(err){
+            console.error('Connection error: ', err);
+            res.send({result: 'error', err: err.code});
+        }
+        else{            
+            connection.query(queryString, values, function(err, rows, fields){
+                connection.release();
+                if(err){
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err: err.code
+                    });
+                }
+                else{
+                    res.send('Success');
+                }
+            });
+        }
+    });
+};
+
 module.exports = pool;
