7a84f64f3732d6be7f823d2ed9589ef5a95a911d
user808d <qlcfz5@mst.edu>
2015-04-28 13:32:34 -0500

    modified routes to use db query method callbacks for async usage

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,62 @@
 var express = require('express');
-var db = require('../database');
 var api = express.Router();
+var db = require('../database');
 
 /* GET fields */
 api.get('/fields/', function(req, res, next){
     var q_string = 'SELECT * FROM Fields';
+
+    db.select(q_string, [], function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
 });
 
 /* POST new field */
 api.post('/fields/', function(req, res, next){
-    var q_string = 'INSERT INTO Fields VALUES(?)';
+    var q_string = 'INSERT INTO Fields(fieldName) VALUES(?)';
     var q_values = Object.keys(req.body).map(function(k){return req.body[k];});
 
-    db.insert(res, q_string, q_values);
+    db.insert(q_string, q_values, function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
+});
+
+/* GET stats by article */
+api.get('/stats/:article_id', function(req, res, next) {
+    var q_string = 'SELECT * FROM Stats S, Types T '
+        + 'WHERE S.typeID = T.typeID AND S.articleID = ?';
+    var q_params = Object.keys(req.params).map(function(k){return req.params(k);});
+
+    db.select(q_string, q_params, function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
+});
+
+/* POST new stat for article */
+api.get('/stats/:article_id', function(req, res, next) {
+    var q_string = 'INSERT INTO Stats(articleID, typeID, tableName) '
+        + 'VALUES(?, ?, ?, ?)';
+    var q_values = Object.keys(req.params).map(function(k){return req.params(k);});
+
+    db.insert(q_string, q_values, function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
 });
 
 /* GET users */
@@ -21,7 +65,13 @@ api.get('/users/', function(req, res, next) {
                 + 'position, website, F.fieldName '
                 + 'FROM Users U, Fields F WHERE U.fieldID = F.fieldID';
 
-    db.select(res, q_string, []);
+    db.select(q_string, [], function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
 });
 
 /* POST new user */
@@ -29,16 +79,42 @@ api.post('/users/', function(req, res, next) {
     var q_string = 'INSERT INTO Users VALUES(?, ?, ?, ?, ?, ?, ?, '
         + '(SELECT fieldID FROM Fields WHERE fieldName = ?))';
     var q_values = Object.keys(req.body).map(function(k){return req.body[k];});
-    
-    db.insert(res, q_string, q_values);
+
+    db.insert(q_string, q_values, function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
 });
 
 /* PUT updated user info */
 api.put('/users/:email', function(req, res, next){
+    var q_string = 'UPDATE ';
+    var q_values = Object.keys(req.params).map(function(k){return req.params(k);});
+
+    db.update(q_string, q_values, function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
 });
 
 /* DELETE user */
 api.delete('/users/:email', function(req, res, next){
+    var q_string = '';
+    var q_params = Object.keys(req.params).map(function(k){return req.params(k);});
+
+    db.del(q_string, q_params, function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
 });
 
 /* GET user info by email */
@@ -46,19 +122,42 @@ api.get('/users/:email', function(req, res, next) {
     var q_string = 'SELECT * FROM Users U, Fields F '
         + 'WHERE F.fieldID = U.fieldID AND U.email = ?';
     var q_params = Object.keys(req.params).map(function(k){return req.params(k);});
-    db.select(res, q_string, q_params);
+
+    db.select(q_string, q_params, function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
 });
 
 /* GET articles */
 api.get('/articles/', function(req, res, next) {
     var q_string = 'SELECT * FROM Articles A, Abstracts Ab '
         + 'WHERE A.articleID = Ab.articleID';
-
-    db.select(res, q_string, []);
+    
+    db.select(q_string, [], function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
 });
 
 /* POST newly authored article */
 api.post('/articles/', function(req, res, next) {
+    var q_string = '';
+    var q_values = Object.keys(req.params).map(function(k){return req.params(k);});
+
+    db.insert(q_string, q_values, function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
 });
 
 /* GET article by id */
@@ -67,11 +166,27 @@ api.get('/articles/:id', function(req, res, next) {
         + 'WHERE A.articleID = Ab.articleID AND A.articleID = ?';
     var q_params = Object.keys(req.params).map(function(k){return req.params(k);});
 
-    db.select(res, q_string, q_params);
+    db.select(q_string, q_params, function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
 });
 
 /* PUT updated article by id */
 api.put('/articles/:id', function(req, res, next) {
+    var q_string = '';
+    var q_values = Object.keys(req.params).map(function(k){return req.params(k);});
+
+    db.update(q_string, q_values, function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
 });
 
 /* GET articles by author (email must be encoded) */
@@ -80,14 +195,26 @@ api.get('/articles/:email', function(req, res, next) {
         + 'WHERE A.articleID = Ab.articleID AND A.authorEmail = ?';
     var q_params = Object.keys(req.params).map(function(k){return req.params(k);});
 
-    db.select(res, q_string, q_params);
+    db.select(q_string, q_params, function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
 });
 
 /* GET edits */
 api.get('/edits/', function(req, res, next) {
     var q_string = 'SELECT * FROM Edits';
 
-    db.select(res, q_string, []);
+    db.select(q_string, [], function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
 });
 
 /* POST user edit to an article */
@@ -99,7 +226,13 @@ api.get('/edits/:email', function(req, res, next) {
     var q_string = 'SELECT * FROM Edits WHERE authorEmail = ?';
     var q_params = Object.keys(req.params).map(function(k){return req.params(k);});
 
-    db.select(res, q_string, q_params);
+    db.select(q_string, q_params, function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
 });
 
 /* GET edits by date */
@@ -107,7 +240,13 @@ api.get('/edits/:date', function(req, res, next) {
     var q_string = 'SELECT * FROM Edits WHERE date = ?';
     var q_params = Object.keys(req.params).map(function(k){return req.params(k);});
 
-    db.select(res, q_string, q_params);
+    db.select(q_string, q_params, function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
 });
 
 /* GET edits by article id */
@@ -115,16 +254,69 @@ api.get('/edits/:article_id', function(req, res, next) {
     var q_string = 'SELECT * FROM Edits WHERE articleID = ?';
     var q_params = Object.keys(req.params).map(function(k){return req.params(k);});
 
-    db.select(res, q_string, q_params);
+    db.select(q_string, q_params, function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
 });
 
-/* GET stats for an article */
-api.get('/stats/:article_id', function(req, res, next) {
-    var q_string = 'SELECT * FROM Stats S, Types T'
-        + 'WHERE S.typeID = T.typeID AND S.articleID = ?';
+/* GET abstract by article id */
+api.get('/abstracts/:article_id', function(req, res, next) {
+    var q_string = 'SELECT * FROM Abstracts WHERE articleID = ?';
     var q_params = Object.keys(req.params).map(function(k){return req.params(k);});
 
-    db.select(res, q_string, q_params);
+    db.select(q_string, q_params, function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
+});
+
+/* POST new abstract for article */
+api.post('/abstracts/:article_id', function(req, res, next) {
+    var q_string = 'INSERT INTO Abstracts(content, articleID) VALUES(?, ?)';
+    var q_params = Object.keys(req.params).map(function(k){return req.params(k);});
+
+    db.insert(q_string, q_params, function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
+});
+
+/* GET urlReferences by articleID */
+api.get('/urlReferences/:article_id', function(req, res, next) {
+    var q_string = 'SELECT * FROM URLReferences WHERE articleID = ?';
+    var q_params = Object.keys(req.params).map(function(k){return req.params(k);});
+
+    db.select(q_string, q_params, function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
+});
+
+/* POST urlReference for article */
+api.post('/urlReferences/:article_id', function(req, res, next) {
+    var q_string = 'INSERT INTO URLReferences VALUES(?,?)';
+    var q_values = Object.keys(req.params).map(function(k){return req.params(k);});
+
+    db.insert(q_string, q_values, function(q_res){
+        if(q_res.result == 'error'){
+            res.statusCode = 500;
+            res.send(q_res);
+        }
+        else res.send(q_res);
+    });
 });
 
 module.exports = api;
