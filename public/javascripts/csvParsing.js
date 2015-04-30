4b18678d0b141a3f4c3ecfa51c9c993fb25fb7a2
user808d <qlcfz5@mst.edu>
2015-04-29 20:53:54 -0500

    script to read csv file after user chooses file to upload

---
+++ public/javascripts/csvParsing.js
@@ -0,0 +1,17 @@
+var data;
+ 
+function handleFileSelect(evt) {
+    var file = evt.target.files[0];
+    
+    Papa.parse(file, {
+        header: true,
+        dynamicTyping: true,
+        complete: function(results) {
+            data = results;
+        }
+    });
+}
+
+$(document).ready(function(){
+    $("#csv-file").change(handleFileSelect);
+});
