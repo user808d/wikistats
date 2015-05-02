20474abe018c255d7869247a1238e4e2a0416c93
user808d <qlcfz5@mst.edu>
2015-05-01 23:59:26 -0500

    add handlebars for displaying articles on the front page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
-  $('[data-toggle="offcanvas"]').click(function () {
-    $('.row-offcanvas').toggleClass('active')
-  });
+    $('[data-toggle="offcanvas"]').click(function () {
+      $('.row-offcanvas').toggleClass('active')
+    });
+    var source = $('#articlesList').html();
+    var template = Handlebars.compile(source);
+    $results = $('#allArticles');
+    
+    $.get('/api/articles', function(data){
+        $results.html(template({articles: data}));
+    });
 });
