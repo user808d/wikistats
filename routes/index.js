4d61d6f76f7ac42687c143a91d254767b2a5a0c4
user808d <qlcfz5@mst.edu>
2015-05-03 11:17:55 -0500

    added auth routes and test route along with test page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,59 @@
 var express = require('express');
 var root = express.Router();
+var db = require('../database');
+var articles = require('./api/articles')(db);
+var users = require('./api/users')(db);
+
+function requireLogin (req, res, next) {
+  if (!req.user) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
 
 /* GET home page. */
-root.get('/', function(req, res, next) {
-    res.render('index', { title: 'Home', site: 'WikiStats' });
+root.get('/', articles.all, function(req, res, next) {
+    res.render('index', {
+        title: 'Home',
+        site: 'WikiStats',
+        articles: req.locals.articles
+    });
 });
+
+root.get('/articles/:articleID', articles.find, function(req, res, next){
+    res.render('articles/show', {
+        title: req.locals.articles[0].title,
+        site: 'WikiStats',
+        article: req.locals.articles[0]
+    });
+});
+
+root.route('/login')
+    .get(function(req, res, next){
+        res.render('login', {
+            title: 'Login',
+            site: 'WikiStats'
+        });
+    })
+    .post(users.auth, function(req, res, next){
+        if(req.session && req.session.user){
+            res.redirect('/');
+        }
+        else{
+            res.render('login', {error: 'Invalid Email or Password'});
+        }
+    });
+
+root.route('/logout')
+    .get(function(req, res, next){
+        res.session.reset();
+        res.redirect('/');
+    });
+
+root.route('/test')
+    .get(function(req, res, next){
+        res.render('test');
+    });
 
 module.exports = root;
