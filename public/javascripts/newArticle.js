e4b864e60f5361c6b998e5d0663bd30b6be563d8
user808d <qlcfz5@mst.edu>
2015-05-05 02:43:00 -0500

    article create

--- public/javascripts/newArticle.js
+++ public/javascripts/newArticle.js
@@ -12,14 +12,68 @@ function handleFileSelect(evt) {
     });
 }
 
+function submitAbstract(params){
+    $.post('/api/abstracts', params, function(res){
+        console.log(res);
+    });
+}
+
+function submitStat(params){
+    $.post('/api/stats', params, function(res){
+        console.log(res);
+    });
+}
+
+function submitCSV(params){
+    $.post('/api/upload', params, function(res){
+        console.log(res);
+    });
+}
+
+function submitURL(params){
+    $.post('/api/urlReferences', params, function(res){
+        console.log(res);
+    });
+}
+
 function publishArticle(evt){
-    var parameters = {};
+    var parameters = {},
+        preHeaders = [],
+        preRows = [];
+    
     parameters.title = $('#title').val();
     parameters.email = $('#email').val();
-
-    /*$.post('/api/articles', parameters, function(data){
-        $.post('/api/abstract/' + 
-    });*/
+    var headerTypes = JSON.parse($('#headerTypes').val());
+    for(var i in data.data[0]){
+        var x = {};
+        x[data.data[0][i]] =  headerTypes[i];
+        preHeaders.push(x);
+    }
+    for(i=1; i < data.data.length; i++)
+        preRows.push(data.data[i]);
+    
+    $.post('/api/articles', parameters, function(res){
+        var newAb = {},
+            newStat = {},
+            newUrl = {},
+            csvUpload = {};
+        console.log(preRows, preHeaders);
+        csvUpload.rows = preRows;
+        csvUpload.headers = preHeaders;
+        newAb.articleID = res.articleID;
+        newStat.articleID = res.articleID;
+        newUrl.articleID = res.articleID;
+        newUrl.urlReference = $('#urlReference').val();
+        newAb.content = $('#content').val();
+        newStat.typeID = $('input[name="group1"]:checked').val();
+        newStat.tableName = $('#tableName').val();
+        csvUpload.title = newStat.tableName;
+        
+        submitAbstract(newAb);
+        submitStat(newStat);
+        submitCSV(csvUpload);
+        submitURL(newUrl);
+    });
 }
 
 $(function(){
